ac7cadf9ad207fefe693a4e5a1c3e510e4d3d528
komakulakavya <komakulakavya23@gmail.com>
2024-09-27 10:01:55 +0530

    two commit

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const cors=require('cors')
 const studentRouter = require('./routes/two')
 
 
-const url = 'mongodb://127.0.0.1:27017/MGIT'
+//const url = 'mongodb://127.0.0.1:27017/MGIT'
+
+
+const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=305'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
