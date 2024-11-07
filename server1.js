e0c9ca166936c62fea12871c888ce883e539bb15
Komakulakavya <125781517+Komakulakavya@users.noreply.github.com>
2024-11-07 23:01:35 +0530

    Add files via upload

---
+++ server1.js
@@ -0,0 +1,46 @@
+const express= require('express')
+const app = express()
+const jwt= require('jsonwebtoken')
+app.use(express.json())
+require('dotenv').config()
+
+const posts=[
+    {
+        name : "cbit",
+        title: "welcome to cbit"
+    },
+    {
+        name : "mgit",
+        title: "welcome to mgit" 
+    }
+]
+
+
+const authenticateToken=(req,res,next)=>{
+    const authHeader=req.headers['authorization']
+    const token= authHeader && authHeader.split(' ')[1]
+    if(!token) return res.sendStatus(401)
+    
+    jwt.verify(toke, process.env.ACCESS_TOKEN, (err,user)=>{
+        if (err){
+            return res.sendStatus(403)
+        }
+        req.user=user
+        next()
+    })
+}
+
+
+app.post('/login', (req,res)=>{
+    const username = req.body.username
+    const user={ name: username}
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+    res.json({accesTokenis : accessToken})
+})
+app.use(authenticateToken)
+app.get('/posts',(req,res)=>{
+    console.log(req.user.name)
+    res.json(posts.filter(post => post.name === req.user.name))
+
+})
+app.listen(3003)
